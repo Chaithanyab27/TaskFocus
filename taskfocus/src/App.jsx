9acf9f46b74ca36608ebe181d49d7bbace678c82
Chaithanyab27 <chaitanyab231@gmail.com>
2025-10-17 11:28:01 +0530

    17th oc Tasklist pt

--- taskfocus/src/App.jsx
+++ taskfocus/src/App.jsx
@@ -2,24 +2,38 @@ import Taskform from "./Components/Taskform";
 import TaskList from "./Components/TaskList";
 import Progresstracker from "./Components/Progresstracker";
 import { useEffect, useState } from "react";
+import "./Style.css";
 
-export default function App()  {
-const [task, setTasks] = useState([]);
+export default function App() {
+  const [tasks, setTasks] = useState([]);
 
-useEffect(() => {
-  localStorage.setItem("tasks", JSON.stringify(tasks))
-});
+  useEffect(() => {
+    localStorage.setItem
+    ("tasks", JSON.stringify(tasks))
+  });
 
   const addTask = (task) => {
-    setTasks([...tasks, task]);
+    setTasks([...tasks,task]);
+  }
+
+  const updateTask = (updatedTask, index) => {
+    const newtask = [...tasks];
+     newtask[index] = updatedTask;
+    setTasks(newtask);
+  }
+
+  const deleteTask = (index) => {
+      setTasks(tasks.filter((_, i) => i != index));
   }
 
   return(
     <div>
       <h1>Task Focus</h1>
-      <p>Our Friendly TaskManager</p>
+      <p>Our friendly TaskManager</p>
       <Taskform addTask = {addTask}/>
-      <TaskList />
+      <TaskList tasks = {tasks} 
+      updateTask = {updateTask}
+      deleteTask = {deleteTask}/>
       <Progresstracker />
       <button>Clear all tasks</button>
     </div>
--- taskfocus/src/Components/Progresstracker.jsx
+++ taskfocus/src/Components/Progresstracker.jsx
@@ -1,4 +1,4 @@
-export default function Progresstracker() {
+export default function Progresstracker(tasks) {
     return(
         <div>
             <h1>Progress Tracker Component</h1>
